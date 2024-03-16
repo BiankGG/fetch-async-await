e87ca4ea72aff0b4ba7f934f20f5b0f5997cab1a
Bianca G. Griffin <Biancagriffin@hotmail.es>
2024-03-16 11:34:57 +0100

    actualizado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-//borrar lista de pokemon y renderizar una pagina de pokemon
+//renderizar una pagina de pokemon
+//capturar detalles url
+//mostrar en dom
 //navegar a la siguiente pagina
 //navegar a la pagina anterior
 //buscador pokemon
 //search
 //get pokemon
 // let currentPage = 1;
-// let pages= null;
+// let pages= null;(web *10)
 //localStorage? :(
 
 const searchButton = document.getElementById("searchBtn");
@@ -23,6 +25,7 @@ const limit = 10;
 const getPokemon = async (page) => {
   const offset = (page - 1) * limit;
   try {
+    //concatenacion
     const url = baseUrl + "?limit=" + limit + "&offset=" + offset;
     const response = await fetch(url);
     if (!response.ok) {
@@ -38,7 +41,7 @@ const getPokemon = async (page) => {
     console.log("error al obtener los datos", error);
   }
 };
-//detalles pokemon, no encuentro info, url no aparece//
+//detalles pokemon, no encuentro info, url no apar+ece//
 const getPokemonInfo = async (url) => {
   try {
     const response = await fetch(url);
@@ -52,10 +55,11 @@ const getPokemonInfo = async (url) => {
   }
 };
 
-//rederizar pokemon y mostrar en DOM
+//renderizar pokemon y mostrar en DOM
 const renderPokemon = (pokemonList) => {
   containerApp.innerHTML = "";
   pokemonList.forEach((pokemon) => {
+    //concatenar con +, sale solo 1 poke con =
     containerApp.innerHTML += `
         <article class='card'>
         <img class= 'card_image' src='${pokemon.sprites.front_default}' alt='${pokemon.name}' />
@@ -65,7 +69,7 @@ const renderPokemon = (pokemonList) => {
         `;
   });
 };
-//button para paginacion
+//button para las paginacion
 prevPageButton.addEventListener("click", () => {
   if (currentPage > 1) {
     currentPage--;
@@ -80,15 +84,15 @@ nextPageButton.addEventListener("click", () => {
 
 getPokemon(currentPage);
 
-//type="text" id="searchInput"//buscar x nombre
+//type="text" id="searchInput"//buscar x nombre//Pokemon no encontrado (error)
 const searchPokemon = async (name) => {
   try {
-    const url = `${baseUrl}/${name.toLowerCase()}`;
+    const url = baseUrl + '/'+ name.toLowerCase();
     const detalles = await getPokemonInfo(url);
     renderPokemon([detalles]);
   } catch (error) {
     console.log("error", error);
-    containerApp.innerHTML = "<p>pokemon no encontrado</p>";
+    containerApp.innerHTML = "<p>Pokemon no encontrado</p>";
   }
 };
 
